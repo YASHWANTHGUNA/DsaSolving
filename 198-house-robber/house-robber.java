2e6a855b85cb45674d9599a36887999a82d2cb62
YASHWANTH. GUNA <mail2yashwanth2004@gmail.com>
2026-03-13 22:30:42 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (14.63%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,18 +1,13 @@
 class Solution {
     public int rob(int[] nums) {
-        int n = nums.length;
-
-        if(n == 1) return nums[0];
-
-        int[] dp = new int[n];
-
-        dp[0] = nums[0];
-        dp[1] = Math.max(nums[0], nums[1]);
-
-        for(int i = 2; i < n; i++) {
-            dp[i] = Math.max(dp[i-1], dp[i-2] + nums[i]);
+        int prev1  =0 ; 
+        int prev2 = 0; 
+        for(int num: nums) {
+            int curr = Math.max(prev1,  prev2 + num); 
+            prev2 = prev1; 
+            prev1 = curr; 
         }
-
-        return dp[n-1];
+        return prev1; 
+        
     }
 }
