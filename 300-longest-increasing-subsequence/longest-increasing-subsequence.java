bb2566cc5183835da992aa2b6cf5921efc30e3f7
YASHWANTH. GUNA <mail2yashwanth2004@gmail.com>
2026-03-16 07:35:51 +0530

    Time: 37 ms (73.30%) | Memory: 45.8 MB (97.12%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -3,16 +3,18 @@ class Solution {
         int n = nums.length;
         int[] dp = new int[n]; 
         Arrays.fill(dp, 1); 
+        int maxLen = 1; 
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < i; j++)  {
                 if(nums[i] > nums[j]) {
                     dp[i] = Math.max(dp[i], dp[j]+1);
                 }
             }
+            maxLen = Math.max(maxLen, dp[i]);
         }
-        Arrays.sort(dp); 
-        return dp[n-1];
 
         
+
+      return maxLen;   
     }
 }
