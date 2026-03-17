2bb30aa64eef2b1e505023eb7121ade0940539d2
YASHWANTH. GUNA <mail2yashwanth2004@gmail.com>
2026-03-17 07:43:54 +0530

    Time: 31 ms (24.04%) | Memory: 47.2 MB (47.64%) - LeetSync

--- 646-maximum-length-of-pair-chain/maximum-length-of-pair-chain.java
+++ 646-maximum-length-of-pair-chain/maximum-length-of-pair-chain.java
@@ -4,6 +4,7 @@ class Solution {
         int n = pairs.length; 
         Arrays.sort(pairs,  (a,b)-> a[0]-b[0]); 
         int[] dp = new int[n]; 
+        int maxLen = 1; 
         Arrays.fill(dp, 1); 
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < i; j++) {
@@ -11,9 +12,9 @@ class Solution {
                     dp[i] = Math.max(dp[i], dp[j]+1); 
                 }
             }
+            maxLen = Math.max(maxLen, dp[i]); 
         }
-        Arrays.sort(dp); 
-        return dp[n-1];
+        return maxLen; 
         
     }
 }
