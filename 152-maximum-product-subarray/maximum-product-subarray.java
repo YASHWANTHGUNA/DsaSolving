69cc848a376ffbb3d2491b4ee27b6a0e96304bd9
YASHWANTH. GUNA <mail2yashwanth2004@gmail.com>
2026-03-26 12:56:25 +0530

    Time: 2 ms (68.76%) | Memory: 47.2 MB (81.90%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.java
+++ 152-maximum-product-subarray/maximum-product-subarray.java
@@ -1,20 +1,19 @@
 class Solution {
     public int maxProduct(int[] nums) {
-        int n  = nums.length; 
-        if(n==1) {
+        int n = nums.length; 
+        if(n==0) {
             return nums[0]; 
         }
-        int maxEndingHere = nums[0];
-        int minEndingHere = nums[0]; 
+        int maxEnding = nums[0]; 
+        int minEnding = nums[0];
         int globalMax = nums[0]; 
         for(int i = 1; i < n; i++) {
-            int tempMax = Math.max(nums[i], Math.max(nums[i] * maxEndingHere, nums[i]*minEndingHere)); 
-            int tempMin = Math.min(nums[i], Math.min(nums[i] * maxEndingHere, nums[i] * minEndingHere));
-            maxEndingHere = tempMax; 
-            minEndingHere = tempMin; 
-            globalMax = Math.max(globalMax, maxEndingHere); 
+            int tempMax = Math.max(nums[i],  Math.max( nums[i] * maxEnding, nums[i]*minEnding)); 
+            int tempMin = Math.min(nums[i], Math.min( nums[i] * maxEnding, nums[i] * minEnding)); 
+            maxEnding = tempMax; 
+            minEnding = tempMin; 
+            globalMax = Math.max(globalMax,  maxEnding); 
         }
         return globalMax; 
-        
     }
 }
