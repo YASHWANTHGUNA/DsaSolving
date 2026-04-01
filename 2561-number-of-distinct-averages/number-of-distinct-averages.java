72ead6709a7981612d80fde18efec94ea7c4743f
YASHWANTH. GUNA <mail2yashwanth2004@gmail.com>
2026-04-01 19:52:09 +0530

    Time: 3 ms (95.39%) | Memory: 42.8 MB (95.75%) - LeetSync

---
+++ 2561-number-of-distinct-averages/number-of-distinct-averages.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int distinctAverages(int[] nums) {
+        Arrays.sort(nums);
+
+        int left = 0, right = nums.length - 1;
+        Set<Integer> set = new HashSet<>();
+
+        while (left < right) {
+            int sum = nums[left] + nums[right];
+            set.add(sum);
+            left++;
+            right--;
+        }
+
+        return set.size();
+        
+    }
+}
