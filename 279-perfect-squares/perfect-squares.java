2b9975f43e8a60ba4a1f14dad42fa8e5e07fed1e
YASHWANTH. GUNA <mail2yashwanth2004@gmail.com>
2026-04-03 12:10:53 +0530

    Time: 35 ms (52.93%) | Memory: 44.1 MB (91.07%) - LeetSync

--- 279-perfect-squares/perfect-squares.java
+++ 279-perfect-squares/perfect-squares.java
@@ -2,7 +2,7 @@ class Solution {
     public int numSquares(int n) {
     int[] dp = new int[n+1]; 
     dp[0] = 0; 
-    for(int i = 0; i <= n; i++) {
+    for(int i = 1; i <= n; i++) {
         dp[i] = i;
     }
      for(int  i = 1; i <= n; i++) {
