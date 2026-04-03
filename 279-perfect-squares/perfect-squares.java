ed96f10bd5fc7807ae8707f414c7b42303fce053
YASHWANTH. GUNA <mail2yashwanth2004@gmail.com>
2026-04-03 12:08:38 +0530

    Time: 33 ms (61.39%) | Memory: 44.7 MB (34.95%) - LeetSync

---
+++ 279-perfect-squares/perfect-squares.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int numSquares(int n) {
+    int[] dp = new int[n+1]; 
+    dp[0] = 0; 
+    for(int i = 0; i <= n; i++) {
+        dp[i] = i;
+    }
+     for(int  i = 0; i <= n; i++) {
+        for(int j = 0; j * j <= i; j++) {
+            dp[i] = Math.min(dp[i], 1 + dp[i-j*j]);
+        }
+     }
+     return dp[n];
+        
+    }
+}
