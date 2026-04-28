469b162462a29b1500c32ea3eae86cc814fdbfbf
YASHWANTH. GUNA <mail2yashwanth2004@gmail.com>
2026-04-28 06:37:35 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (24.16%) - LeetSync

---
+++ 413-arithmetic-slices/arithmetic-slices.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int numberOfArithmeticSlices(int[] nums) {
+        int n = nums.length; 
+        int[] dp = new int[n]; 
+        if(n < 3) return 0; 
+        
+        int sum = 0; 
+        for(int i = 2; i  < n; i++) {
+            if(nums[i]-nums[i-1] == nums[i-1]-nums[i-2]) {
+                dp[i] = dp[i-1] + 1; 
+            } else {
+                dp[i] = 0; 
+            }
+            sum += dp[i]; 
+        }
+        return sum; 
+        
+    }
+}
