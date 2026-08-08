b2ad2ff17ff4679a7936d742bb870575e13e9f1a
YASHWANTH. GUNA <mail2yashwanth2004@gmail.com>
2026-08-08 19:43:55 +0530

    Time: 0 ms (100.00%) | Memory: 23.3 MB (75.70%) - LeetSync

---
+++ 890-lemonade-change/lemonade-change.py
@@ -0,0 +1,27 @@
+class Solution:
+    def lemonadeChange(self, bills):
+        n = len(bills)
+        count5 = 0
+        count10 = 0
+
+        for bill in bills:
+            if bill == 5:
+                count5 += 1
+
+            elif bill == 10:
+                if count5 > 0:
+                    count5 -= 1
+                    count10 += 1
+                else:
+                    return False
+
+            else:
+                if count10 > 0 and count5 > 0:
+                    count10 -= 1
+                    count5 -= 1
+                elif count5 >= 3:
+                    count5 -= 3
+                else:
+                    return False
+
+        return True
